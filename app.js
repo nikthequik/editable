cc9ff56a8cd6a2556f5f3d05e1ece45d39695f72
nikthequik <quik.the.nick@gmail.com>
2016-08-10 12:10:21 -0600

    -create editable directive and place it on two elements in index.html

--- app.js
+++ app.js
@@ -2,6 +2,25 @@ angular.module('editable', [])
 .directive('makeEditable', function() {
 	return {
 		restrict: 'A',
-		
+		templateUrl: 'editable.html',
+		transclude: true,
+		scope: true,
+		link: function(scope, el, attrs) {
+			scope.editing = false;
+			el.css({paddingTop: '0.6em', position: 'relative'});
+
+			scope.toggleEditMode = function() {
+				scope.editing = !scope.editing;
+				scope.editButton = el.find('button.editButton');
+				
+				if (scope.editing == true) {
+					scope.editButton.siblings('*').attr('contenteditable', 'true');
+					scope.editButton.text('Save');
+				} else {
+					scope.editButton.siblings('*').attr('contenteditable', 'false');
+					scope.editButton.text('Edit');
+				}
+			}
+		}
 	}
 });
